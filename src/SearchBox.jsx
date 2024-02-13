73b13403dbed627ee1025f7015b4886dc9816291
Dimple <dsaraogi2000@gmail.com>
2024-02-13 14:51:16 +0545

    add search form

---
+++ src/SearchBox.jsx
@@ -0,0 +1,37 @@
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./SearchBox.css";
+import { useState } from "react";
+
+export default function SearchBox() {
+  let [city, setCity] = useState("");
+
+  let handleChange = (evt) => {
+    setCity(evt.target.value);
+  };
+
+  let handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(city);
+  };
+
+  return (
+    <div className="SearchBox">
+      <h1>Search for the weather</h1>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id="city"
+          label="City Name"
+          variant="outlined"
+          required
+          value={city}
+          onChange={handleChange}
+        />
+        <br /> <br />
+        <Button variant="contained" type="submit">
+          Search
+        </Button>
+      </form>
+    </div>
+  );
+}
