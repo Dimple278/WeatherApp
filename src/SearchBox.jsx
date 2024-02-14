28e03abace87a6ee045f98c46343d20e81beb75d
Dimple <dsaraogi2000@gmail.com>
2024-02-14 10:12:10 +0545

    use weather API

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -3,8 +3,28 @@ import Button from "@mui/material/Button";
 import "./SearchBox.css";
 import { useState } from "react";
 
+const API_URL = import.meta.env.VITE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 export default function SearchBox() {
   let [city, setCity] = useState("");
+  let getWeatherInfo = async () => {
+    let response = await fetch(
+      `${API_URL}?q=${city}&appid=${API_KEY}&units=metric`
+    );
+    let jsonResponse = await response.json();
+
+    let result = {
+      city: city.toLocaleUpperCase(),
+      feelsLike: jsonResponse.main.feels_like,
+      humidity: jsonResponse.main.humidity,
+      temp: jsonResponse.main.temp,
+      tempMax: jsonResponse.main.temp_max,
+      tempMin: jsonResponse.main.temp_min,
+      weather: jsonResponse.weather[0].description,
+    };
+    console.log(result);
+  };
 
   let handleChange = (evt) => {
     setCity(evt.target.value);
@@ -13,6 +33,8 @@ export default function SearchBox() {
   let handleSubmit = (evt) => {
     evt.preventDefault();
     console.log(city);
+    setCity("");
+    getWeatherInfo();
   };
 
   return (
